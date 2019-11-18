6fafc61c33165229b232a496cef1bab08e185d00
Jibran Khan <jibrankhan4576@gmail.com>
2019-11-18 16:37:52 +0530

    changes

---
+++ Feature_Creation/feature_project_a.py
@@ -0,0 +1,8 @@
+#%%
+def func_1 ():
+    print("This is the first function")
+
+def func_2():
+    print("This is the second function")
+
+# %%
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import 
+#%%
+import json
+from EDA.eda_project_a import binary_plot
+from Feature_Creation.feature_project_a import func_1,func_2
+
+
+if __name__ == "__main__":
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+    A = config['EDA_var']['Column1']
+    B = config['EDA_var']['Column2']
+    print(A)
+    print(B)
+    binary_plot(A,B)
+    func_1()
+    func_2()
+    
+
+# %%
